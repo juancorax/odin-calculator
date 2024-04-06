7831ad4a487da76051f570d63dc91146818aa9ff
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-04-06 13:47:35 -0300

    feat: add a function that calls the operation ones

--- script.js
+++ script.js
@@ -14,6 +14,26 @@ function divide(a, b) {
   return a / b;
 }
 
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "+":
+      add(firstNumber, secondNumber);
+      break;
+
+    case "-":
+      subtract(firstNumber, secondNumber);
+      break;
+
+    case "*":
+      multiply(firstNumber, secondNumber);
+      break;
+
+    case "/":
+      divide(firstNumber, secondNumber);
+      break;
+  }
+}
+
 let firstNumber;
 let operator;
 let secondNumber;
