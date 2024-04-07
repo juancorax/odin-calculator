0b8a917b330a07e69235fdac989ebaecca013141
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-04-07 08:26:41 -0300

    feat: populate the display when you click numbers
    Fix the function that calls the operation ones.

--- script.js
+++ script.js
@@ -17,23 +17,104 @@ function divide(a, b) {
 function operate(operator, firstNumber, secondNumber) {
   switch (operator) {
     case "+":
-      add(firstNumber, secondNumber);
-      break;
+      return add(firstNumber, secondNumber);
 
     case "-":
-      subtract(firstNumber, secondNumber);
-      break;
+      return subtract(firstNumber, secondNumber);
 
     case "*":
-      multiply(firstNumber, secondNumber);
-      break;
+      return multiply(firstNumber, secondNumber);
 
     case "/":
-      divide(firstNumber, secondNumber);
-      break;
+      return divide(firstNumber, secondNumber);
   }
+}
+
+function updateDisplay(value) {
+  const display = document.querySelector("#display");
+
+  display.textContent = value;
+  firstNumber = value;
 }
 
 let firstNumber;
 let operator;
 let secondNumber;
+
+const columnContainer = document.querySelector("#column-container");
+
+columnContainer.addEventListener("click", (event) => {
+  let target = event.target;
+
+  switch (target.id) {
+    case "clear-entry":
+      break;
+
+    case "clear-all":
+      break;
+
+    case "backspace":
+      break;
+
+    case "division":
+      break;
+
+    case "number7":
+      updateDisplay("7");
+      break;
+
+    case "number8":
+      updateDisplay("8");
+      break;
+
+    case "number9":
+      updateDisplay("9");
+      break;
+
+    case "multiplication":
+      break;
+
+    case "number4":
+      updateDisplay("4");
+      break;
+
+    case "number5":
+      updateDisplay("5");
+      break;
+
+    case "number6":
+      updateDisplay("6");
+      break;
+
+    case "subtraction":
+      break;
+
+    case "number1":
+      updateDisplay("1");
+      break;
+
+    case "number2":
+      updateDisplay("2");
+      break;
+
+    case "number3":
+      updateDisplay("3");
+      break;
+
+    case "addition":
+      break;
+
+    case "change-sign":
+      break;
+
+    case "number0":
+      updateDisplay("0");
+      break;
+
+    case "decimal-point":
+      break;
+
+    case "result":
+      break;
+  }
+});
